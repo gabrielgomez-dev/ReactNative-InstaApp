6bb65fc897496ecaf5d91c6390d75336bbe5f291
Gabriel Gomez <pc866794@gmail.com>
2024-02-28 18:04:25 -0300

    first commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,9 @@
+const colors = {
+  primary: '#405de6',
+  white: '#fff',
+  black: '#000',
+  gray: '#666',
+  lightgray: '#ccc',
+};
+
+export default colors;
---
+++ src/theme/fonts.ts
@@ -0,0 +1,16 @@
+export const size = {
+  xs: 12,
+  sm: 14,
+  normal: 16,
+  md: 18,
+  lg: 22,
+  xl: 24,
+};
+
+export const weight = {
+  full: '900',
+  semi: '600',
+  thin: '400',
+  bold: 'bold',
+  normal: 'normal',
+};
